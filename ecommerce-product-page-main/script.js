964fcc470e73c675b6d914650b23a4ce3c0d2e1f
Leo1705 <leopetkovski12345@gmail.com>
2021-11-28 16:26:11 +0100

    Initial commit

--- ecommerce-product-page-main/script.js
+++ ecommerce-product-page-main/script.js
@@ -1,10 +1,20 @@
-var image = document.getElementById('image-1');
+var image_active = document.getElementById('image-down');
+var image1 = document.getElementById('image-1');
 var image2 = document.getElementById('image-2');
 var image3 = document.getElementById('image-3');
 var image4 = document.getElementById('image-4');
-image.addEventListener('click', function(){
-  
+image1.addEventListener('click', function(){
+  image_active.src = "images/image-product-1.jpg";
   //for the image select the main image and then just change the src of the main image according to the event listener of the small images
+})
+image2.addEventListener('click', function(){
+    image_active.src = "images/image-product-2.jpg";
+})
+image3.addEventListener('click', function(){
+    image_active.src = "images/image-product-3.jpg";
+})
+image4.addEventListener('click', function(){
+    image_active.src = "images/image-product-4.jpg";
 })
 
  var minus = document.getElementById('minus');
@@ -35,4 +45,5 @@ var cart = document.querySelector('.cart-logo');
 var cart2  = document.querySelector('.cart');
 cart.addEventListener('click', function(){
     cart2.classList.toggle('cart');
+    cart2.classList.toggle('cart-item');
 })
